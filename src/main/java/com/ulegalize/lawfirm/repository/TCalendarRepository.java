3409ba35417dd8cc967621138c6732108b16db53
fumajul <julien.fumanti@finauxa.com>
2022-02-06 11:11:26 +0100

    upgrade ulegalizeshare

--- src/main/java/com/ulegalize/lawfirm/repository/TCalendarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ulegalize.lawfirm.repository;
-
-import com.ulegalize.lawfirm.model.entity.TCalendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface TCalendarRepository extends JpaRepository<TCalendar, Integer>, JpaSpecificationExecutor<TCalendar> {
-
-}
